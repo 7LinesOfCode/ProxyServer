ac2e7d76abdffa1545766e0547ce9d2af42c4d94
k.chmielinski <k.chmielinski@godeltech.com>
2024-10-14 08:51:39 +0200

    Add logic topic No. 7

--- src/server/app.js
+++ src/server/app.js
@@ -14,7 +14,22 @@ const url = `${apiBaseUrl}?start_date=2024-10-07&api_key=${apiKey}`;
 app.get("/api/meteors", async (req, res) => {
   try {
     const response = await axios.get(url);
-    res.json(response.data);
+
+    const meteors = response.data.near_earth_objects["2024-10-07"].map(
+      (meteor) => ({
+        id: meteor.id,
+        name: meteor.name,
+        meters_diameter:
+          meteor.estimated_diameter.meters.estimated_diameter_max,
+        is_potentially_hazardous_asteroid:
+          meteor.is_potentially_hazardous_asteroid,
+        close_approach_date_full: meteor.close_approach_date_full,
+        relative_velocity:
+          meteor.close_approach_data[0].relative_velocity.kilometers_per_second,
+      })
+    );
+
+    res.json(meteors);
   } catch (error) {
     console.error("Error fetching meteors data:", error);
     res.status(500).json({ message: "Error during getting a response." });
